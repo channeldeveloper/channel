e26aaaa274889bff0022a5984a932d47563a9c22
franzsong <franzsong@163.com>
2013-01-11 15:05:23 +0800

    sxy

---
+++ pips-main-launch/src/com/original/main/Main.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.original.main;
+
+
+/**
+ * 启动程序：1）读取系统配置、2）创建FrameworkImpl
+ */
+public final class Main
+{
+
+	
+	/**
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception
+	{	
+	}
+	
+	static boolean DEBUG = false;
+	
+	
+}
