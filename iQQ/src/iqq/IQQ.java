c57f4391b04125b8a4fe56e3b45c5ebe5b8f4635
franzsong <franzsong@163.com>
2013-01-19 22:54:35 +0800

    sxy

---
+++ iQQ/src/iqq/IQQ.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iqq;
+
+/**
+ *
+ * @author sxy
+ */
+public class IQQ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
